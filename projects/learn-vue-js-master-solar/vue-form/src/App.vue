e21a432d0e0202cd98b7dc4c038553af41a20ceb
blossun <blossunn@gmail.com>
2022-01-18 22:35:21 +0900

    submit 이벤트 처리

--- projects/learn-vue-js-master-solar/vue-form/src/App.vue
+++ projects/learn-vue-js-master-solar/vue-form/src/App.vue
@@ -1,20 +1,32 @@
 <template>
-  <form action="">
+  <form v-on:submit="submitForm">
     <div>
       <!-- label은 지정한 for에 해당하는 id값을 바라보게 된다. 어떤 태그를 위한 label인지를 표시-->
       <label for="username">id: </label>
-      <input id="username" type="text">
+      <input id="username" type="text" v-model="username">
     </div>
     <div>
       <label for="password">pw: </label>
-      <input id="password" type="password">
+      <input id="password" type="password" v-model="password">
     </div>
-    <button>login</button>
+    <button type="submit">login</button>
   </form>
 </template>
 
 <script>
 export default {
+  data: function() {
+    return {
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    submitForm: function(event) {
+      event.preventDefault(); //event 기본동작인 새로고침 막기
+      console.log(this.username, this.password);
+    }
+  }
 
 }
 </script>
