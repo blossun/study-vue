1e3b5eb2c0d3941e00edbc976aa5d6f0877e8f26
blossun <blossunn@gmail.com>
2022-01-30 23:55:48 +0900

    example: 헬퍼 함수가 주는 간편함을 확인하기 위한 예제(1) - 기존 코드

---
+++ projects/vue-todo/src/components/Demo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="root">
+      <p>{{ this.$store.getters.originalPrice }}</p>
+      <p>{{ this.$store.getters.doublePrice }}</p>
+      <p>{{ this.$store.getters.triplelPrice }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
