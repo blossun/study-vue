f2dbd33dad39bc4226c49cae18ecfd39853d81a6
blossun <blossunn@gmail.com>
2022-01-30 22:48:22 +0900

    refactor: mapGetters 적용

--- projects/vue-todo/src/components/TodoList.vue
+++ projects/vue-todo/src/components/TodoList.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
         <transition-group name="list" tag="ul">
-          <li v-for="(todoItem, index) in this.todoItems" v-bind:key="todoItem.item" class="shadow">
+          <li v-for="(todoItem, index) in this.storedTodoItems" v-bind:key="todoItem.item" class="shadow">
               <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}" 
                     v-on:click="toggleComplete(todoItem, index)"></i>
               <span v-bind:class="{textCompleted: todoItem.completed}">{{ todoItem.item }}</span>
@@ -14,6 +14,8 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
     methods: {
         removeTodo(todoItem, index) {
@@ -26,9 +28,7 @@ export default {
         }
     },
     computed: {
-        todoItems() {
-            return this.$store.getters.storedTodoItems;
-        }
+        ...mapGetters(['storedTodoItems'])
     }
 }
 </script>
