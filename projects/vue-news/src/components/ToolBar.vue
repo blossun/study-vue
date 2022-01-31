b2ce88f01100a34df1d5e1afe892156b157850e2
blossun <blossunn@gmail.com>
2022-02-01 02:18:01 +0900

    style: ToolBar의 라우터 링크 스타일링

--- projects/vue-news/src/components/ToolBar.vue
+++ projects/vue-news/src/components/ToolBar.vue
@@ -1,17 +1,22 @@
 <template>
-  <div>
-      <router-link to="/news">News</router-link>
-      <router-link to="/ask">Ask</router-link>
+  <div class="header">
+      <router-link to="/news">News</router-link> |
+      <router-link to="/ask">Ask</router-link> |
       <router-link to="/jobs">Jobs</router-link>
   </div>
 </template>
 
-<script>
-export default {
-
+<style scoped>
+.header {
+    color: white;
+    background-color: #42b883;
+    display: flex;
+    padding: 8px;
 }
-</script>
-
-<style>
-
+.header .router-link-exact-active { /* 클릭한 링크 색상 표시 */
+    color: #35495e;
+}
+.header a { /* 활성화되지 않은 링크 표시 */
+    color: white;
+}
 </style>
