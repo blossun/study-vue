13e040b970486aa60d5a7caf59887ed83fcd119c
blossun <blossunn@gmail.com>
2022-02-06 23:46:14 +0900

    context의 commit과 response의 data를 바로 인자로 받아서 사용

--- projects/vue-news/src/store/index.js
+++ projects/vue-news/src/store/index.js
@@ -31,14 +31,18 @@ export const store = new Vuex.Store({
                     console.log(error);
                 });
         },
-        FETCH_JOBS(context) {
+        FETCH_JOBS({ commit }) {
             fetchJobsList()
-                .then(response => context.commit('SET_JOBS', response.data))
+                .then(({ data }) => {
+                    commit('SET_JOBS', data)
+                })
                 .catch(error => console.log(error));
         },
-        FETCH_ASK(context) {
+        FETCH_ASK({ commit }) {
             fetchAskList()
-                .then(response => context.commit('SET_ASK', response.data))
+                .then(({ data }) => {
+                    commit('SET_ASK', data)
+                })
                 .catch(error => console.log(error));
         }
     }
