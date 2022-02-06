59d4503148a83ee2f91a41217477675dee062fa7
blossun <blossunn@gmail.com>
2022-02-06 21:12:08 +0900

    NewsView에 actions와 mutations 적용

--- projects/vue-news/src/views/NewsView.vue
+++ projects/vue-news/src/views/NewsView.vue
@@ -1,28 +1,13 @@
 <template>
   <div>
-      <div v-for="user in users" v-bind:key="user.id">{{ user.title }}</div>
+      <div v-for="user in this.$store.state.news" v-bind:key="user.id">{{ user.title }}</div>
   </div>
 </template>
 
 <script>
-import { fetchNewsList } from '../api/index.js'
-
 export default {
-    data() {
-        return {
-            users: []
-        }
-    },
     created() {
-        console.log('호출 전: ', this);
-        fetchNewsList()
-        .then(response => {
-            console.log('호출 후: ', this);
-            this.users = response.data;
-        })
-        .catch(function(error) {
-            console.log(error);
-        });
+      this.$store.dispatch('FETCH_NEWS');
     },
 }
 </script>
