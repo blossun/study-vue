5d62e44107ae2db3732d67514042d2e82ad51cca
blossun <blossunn@gmail.com>
2022-02-08 00:34:47 +0900

    라우터 params을 이용한 User 상세 페이지 구현

--- projects/vue-news/src/views/UserView.vue
+++ projects/vue-news/src/views/UserView.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
-      user
+    <p>name : {{ this.$store.state.user.id}}</p>
+    <p>karma : {{ this.$store.state.user.karma}}</p>
+    <p>created : {{ this.$store.state.user.created}}</p>
   </div>
 </template>
 
 <script>
 export default {
-
+  created() {
+    const userName = this.$route.params.id;
+    console.log(userName);
+    this.$store.dispatch('FETCH_USER', userName);
+  }
 }
 </script>
 
