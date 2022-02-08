eed69c895d8dc7b9a6e48914a6ff5f1e7eff92e3
blossun <blossunn@gmail.com>
2022-02-08 15:16:19 +0900

    style: 질문 상세 페이지 스타일링 및 v-html 디렉티브 적용

--- projects/vue-news/src/views/ItemView.vue
+++ projects/vue-news/src/views/ItemView.vue
@@ -1,12 +1,33 @@
 <template>
   <div>
-      <p>{{ fetchedItem.title }}</p>
-      <div>{{ fetchedItem.content }}</div>
+    <section>
+      <!-- 질문 상세 정보 -->
+      <div class="user-container">
+        <div>
+          <!-- 사용자 프로필 -->
+          <i class="fas fa-user"></i>
+        </div>
+        <div class="user-description">
+          <!-- 사용자 정보 -->
+          <router-link to="`/user/${fetchedItem.user}`">
+            {{ fetchedItem.user }}
+          </router-link>
+          <div class="time">
+            {{ fetchedItem.time_ago }}
+          </div>
+        </div>
+      </div>
+      <h2>{{ fetchedItem.title }}</h2>
+    </section>
+    <section>
+      <!-- 질문 댓글 -->
+      <div v-html="fetchedItem.content"></div>
+    </section>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import {mapGetters} from 'vuex';
 
 export default {
   computed: {
@@ -20,6 +41,25 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.user-container {
+  display: flex;
+  align-items: center;
+}
 
+.fa-user {
+  font-size: 2.5rem;
+}
+
+.user-description {
+  padding-left: 8px;
+}
+
+.time {
+  font-size: 0.7rem;
+}
+
+.user-container {
+  padding: 0.5rem;
+}
 </style>
